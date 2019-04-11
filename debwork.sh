c6659ffe7ae11985dd207942589ff873306ea65c
James Shane <jamesshane@gmail.com>
2019-04-10 21:14:07 -0600

    added -y's

--- debwork.sh
+++ debwork.sh
@@ -6,13 +6,13 @@ chmod +x linuxReset.sh
 
 ./linuxReset.sh makestage stage1
 
-apt --download-only install gnome-core terminator ufw vnc4server git
+apt --download-only -y install gnome-core terminator ufw vnc4server git 
 
-apt  install -f /var/cache/apt/archives/*.deb
+apt  install -y -f /var/cache/apt/archives/*.deb
 
 wget https://github.com/Automattic/simplenote-electron/releases/download/v1.5.0/Simplenote-linux-1.5.0-amd64.deb
 
-apt install -f ./Simplenote-linux-1.5.0-amd64.deb
+apt install -y -f ./Simplenote-linux-1.5.0-amd64.deb
 
 curl -sL https://deb.nodesource.com/setup_11.x | bash -
 
@@ -22,7 +22,7 @@ wget https://go.microsoft.com/fwlink/?LinkID=760868
 
 mv index.html\?LinkID\=760868 code.deb
 
-apt install -f ./code.deb
+apt install -y -f ./code.deb
 
 passwd jamesshane
 
@@ -30,7 +30,7 @@ ufw enable
 
 ufw allow OpenSSH
 
-apt-get install dirmngr --install-recommends
+apt-get install -y dirmngr --install-recommends
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
@@ -45,3 +45,8 @@ systemctl start mongod
 systemctl enable mongod
 
 npm i -g create-react-app
+
+apt update
+
+apt -y upgrade
+
