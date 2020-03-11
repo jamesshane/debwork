68753f289343b5070915e2bca63931818123b03e
James Shane <jamesshane@gmail.com>
2020-03-11 03:28:54 -0600

    add gitstatus to powerline

--- midnight.sh
+++ midnight.sh
@@ -252,6 +252,36 @@ if [ "$1" == "powerline" ]; then
                 #in .tmux.conf
                 #source /usr/local/lib/python2.7/dist-packages/powerline/bindings/tmux/powerline.conf
                 #set-option -g default-terminal "screen-256color"
+        sudo sh -c 'pip install powerline-gitstatus'
+                #in /usr/local/lib/python2.7/dist-packages/powerline/config_files/colorschemes/default.json
+                #"gitstatus":                 { "fg": "gray8",           "bg": "gray2", "attrs": [] },
+                #"gitstatus_branch":          { "fg": "gray8",           "bg": "gray2", "attrs": [] },
+                #"gitstatus_branch_clean":    { "fg": "green",           "bg": "gray2", "attrs": [] },
+                #"gitstatus_branch_dirty":    { "fg": "gray8",           "bg": "gray2", "attrs": [] },
+                #"gitstatus_branch_detached": { "fg": "mediumpurple",    "bg": "gray2", "attrs": [] },
+                #"gitstatus_tag":             { "fg": "darkcyan",        "bg": "gray2", "attrs": [] },
+                #"gitstatus_behind":          { "fg": "gray10",          "bg": "gray2", "attrs": [] },
+                #"gitstatus_ahead":           { "fg": "gray10",          "bg": "gray2", "attrs": [] },
+                #"gitstatus_staged":          { "fg": "green",           "bg": "gray2", "attrs": [] },
+                #"gitstatus_unmerged":        { "fg": "brightred",       "bg": "gray2", "attrs": [] },
+                #"gitstatus_changed":         { "fg": "mediumorange",    "bg": "gray2", "attrs": [] },
+                #"gitstatus_untracked":       { "fg": "brightestorange", "bg": "gray2", "attrs": [] },
+                #"gitstatus_stashed":         { "fg": "darkblue",        "bg": "gray2", "attrs": [] },
+                #"gitstatus:divider":         { "fg": "gray8",           "bg": "gray2", "attrs": [] }
+                #in /usr/local/lib/python2.7/dist-packages/powerline/config_files/themes/shell/default_leftonly.json
+                # {
+                #        "function": "powerline_gitstatus.gitstatus",
+                #        "priority": 40
+                #},
+                #in /usr/local/lib/python2.7/dist-packages/powerline/config_files/config.json
+                # {
+                #"ext": {
+                #        "shell": {
+                #                "theme": "default_leftonly"
+                #                }
+                #        }
+                #}
+  
 fi
 
 if [ "$1" == "full" ]; then
