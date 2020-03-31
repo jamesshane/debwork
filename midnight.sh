e053d4735da43c3b86953d72b2dbfc6e648e1b1a
James Shane <jamesshane@gmail.com>
2020-03-31 09:26:42 -0600

    added vim fo vim utils

--- midnight.sh
+++ midnight.sh
@@ -81,6 +81,28 @@ if [ "$1" == "fixclock" ]; then
 	echo "don't need this... it's on, if clock is wrong, correct the /etc/localtime and reboot"
 fi
 
+if [ "$1" == "vim" ]; then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+        echo -e "\nsyntax on\nexecute pathogen#infect()\n" >> ~/.vimrc
+
+        git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+        git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
+        cd ~/.vim/bundle && \
+        git clone https://github.com/scrooloose/nerdtree
+
+        cd ~/.vim/bundle && \
+        git clone https://github.com/evandotpro/nerdtree-chmod
+
+        cd ~/.vim/bundle && \
+        git clone https://github.com/low-ghost/nerdtree-fugitive
+
+        cd ~/.vim/bundle && \
+        git clone https://github.com/tpope/vim-fugitive
+fi
+
+
 if [ "$1" == "neofetch" ]; then
 	sudo add-apt-repository ppa:dawidd0811/neofetch-daily -y
         sudo apt update
